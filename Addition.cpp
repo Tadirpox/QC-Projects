c1a999024c4c452200ce29461e82f01fdf2fc4f4
Ailbe Lim <163487198+Tadirpox@users.noreply.github.com>
2025-06-25 17:51:18 -0400

    Updated Addition by Strings

--- Addition.cpp
+++ Addition.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 //Simulate addition with strings
+string add(string num1, string num2){
+    string result;
+    int sum=0;
+    int carry =0;
+    string maxnum= num1;
+    string minnum= num2;
+    int max= num1.length()-1;
+    int min= num2.length()-1;
+    if(min>max){
+        max=num2.length()-1;
+        maxnum= num2;
+        minnum= num1;
+        min=num1.length()-1;
+    }
+    for(int i= max; i>=0; i--){
+        
+        sum= sum+maxnum[i]-48+carry;
+        carry=0;
+        if(min>=0){
+            sum+=minnum[min]-48;
+            min--;
+        }
+        carry= sum/10;
+        char single=(sum%10)+'0';
+        sum=0;
+        
+        result.insert(0,1,single);
+    }
+    if(carry>0){
+        result.insert(0,1,'1');
+    }
+    return result;
+}
 
 int main(){
-    string one, two;
+
+    char x[100];
+    char y[100];
 
     cout<<"Enter two numbers for the addition:"<<endl;
-    cin>>one>>two;
+    cin>>x>>y;
 
-    string max= one, min= two;
+    string result= add(x,y);
+    cout<<result;
 
-    if(one.length()<two.length()){
-        max=two;
-        min=one;
-    }
-
-    int sum= 0;
-    int maxnum[max.length()] ={};
-    int minnum[max.length()] ={};
-
-    for(int i=max.length()-1; i>=0; i--){
-        maxnum[i]=max[i]-=48;
-    }
-    cout<<endl;
-    int i=max.length()-1;
-    for(int r=min.length()-1; r>=0; r--){
-        minnum[i]=min[r]-=48;
-        i--;
-    }
-
-    for(int i=0; i<max.length(); i++){
-        sum= (10*sum)+minnum[i]+maxnum[i];
-    }
-    cout<<"The Sum of these two numbers is "<<sum;
-
-    return 0;
 }
